91abbc8d2ad0ee34c6f92d0a37b2296a1386e629
Jørgen Borgesen <joergen.borgesen@gmail.com>
2016-04-17 16:46:28 +0200

    Add event to IFTT when opening / closing

--- api.js
+++ api.js
@@ -1,36 +1,60 @@
 const express = require('express')
-const Gpio = require('onoff').Gpio;
+const garage = require('./garage');
 
 const app = express();
 
-const trigger = new Gpio(11, 'out');
-const sensor = new Gpio(7, 'in', 'both');
-
-function singlePulse(pin, duration, callback) {
-	pin.write(1, () => {
-		setTimeout(() => {
-			pin.write(0, callback);
-		}, duration);
-	});
-}
-
-// 
 app.get('/garage-door', (req, resp) => {
-	sensor.read((err, value) => {
-		if (err) {
-			resp.status(500).send('Unable to read sensor');
-		} else {
-			resp.send({open: Boolean(value)});
-		}
-	});
+	garage.isOpen()
+		.then(value => resp.send({open: Boolean(value)}))
+		.catch(() => resp.status(500).send('Unable to read sensor'));
 });
-
 
 // Handle requests to trigger port
 app.post('/garage-door/trigger', (req, resp) => {
-	singlePulse(trigger, 200, () => {
-		resp.send('done');
-	});
+	garage.toggle()
+		.then(() => resp.send('done'));
 });
+
+
+// Setup watching for changes
+const http = require('http');
+const subscription = garage.watch().subscribe(value => {
+	const apiToken = process.env.MAKER_IFTTT_API_TOKEN;
+
+	if (!apiToken) {
+		throw new Error('API token missing');
+	}
+
+	const event = value ? 'GARAGE_OPENED' : 'GARAGE_CLOSED';
+
+	console.log('EVENT: ' + value);
+
+	Observable.create(function (o) {
+		const request = http.request({
+			protocol: 'https',
+			hostname: 'maker.ifttt.com',
+			path: '/trigger/' + event + '/with/key/' + apiToken
+			port: 80
+		}, response => {
+			o.next(response.statusCode);
+			o.complete();
+		});
+
+		request.on('error', err => o.error(err));
+
+  }).retryWhen(function (attempts) {
+		// Retry after 1, 30, 60, 300, 600 seconds
+    return Observable.of(1, 30, 60, 300, 600).zip(attempts, i => i).flatMap(i => {
+      console.log("delay retry by " + i + " second(s)");
+      return Observable.timer(i * 1000);
+  	});
+	}).subscribe();
+});
+
+process.on('SIGINT', () => {
+	subscription.unsubscribe();
+	garage.release();
+	process.exit();
+})
 
 module.exports = app;
---
+++ garage.js
@@ -0,0 +1,69 @@
+const Gpio = require('onoff').Gpio;
+
+const trigger = new Gpio(11, 'out');
+const sensor = new Gpio(7, 'in', 'both');
+const Observable = require('rxjs').Observable;
+
+/**
+ * Constructor
+ *
+ * Setup GPIO stuff and add convienient method to query / execute actions
+ */
+function Garage() {
+	const trigger = new Gpio(11, 'out');
+	const sensor = new Gpio(7, 'in', 'both');
+
+	// Release resources
+	function release() {
+		trigger.unexport();
+		sensor.unexport();
+	}
+
+	function isOpen() {
+		return new Promise((resolve, reject) => {
+				sensor.read((err, value) => {
+					if (err) {
+						reject(err);
+						return;
+					}
+					return Boolean(value);
+				});
+		});
+	}
+
+	function toggle() {
+		return new Promise(resolve => {
+			trigger.write(1, () => {
+				setTimeout(() => {
+					pin.write(0, resolve);
+				}, 200);
+			});
+		});
+	}
+
+	function watch() {
+		return Observable.create(observer => {
+			const watcher = (err, value) => {
+					if (err) {
+						observer.error(err);
+						return;
+					}
+					observer.next(value);
+			}
+
+			sensor.watch(watcher);
+
+			return () => {
+				sensor.unwatch(watcher);
+			}
+		})
+		.debounceTime(500);
+	}
+
+	// Expose methods
+	this.release = release;
+	this.isOpen = release;
+	this.toggle = toggle;
+}
+
+module.exports = new Garage();
